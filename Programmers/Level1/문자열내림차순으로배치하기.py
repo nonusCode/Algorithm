467796ee5c0a0ad57a09364b93f8c7a157d8abb7
nonusCode <nonuscode@gmail.com>
2020-12-09 21:59:31 +0900

    Level1

---
+++ Programmers/Level1/문자열내림차순으로배치하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    sol = []
+    for i in s:
+        sol.append(i)
+
+    sol = sorted(sol, reverse=True)
+    res = "".join(sol)
+    
+    return res
+
+# 1
+s = "Zbcdefg"
+
+print(solution(s))

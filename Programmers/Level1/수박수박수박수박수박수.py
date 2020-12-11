7d2415c43c6f7992e8e9267c9c947ff2310f09fa
nonusCode <nonuscode@gmail.com>
2020-12-11 22:56:51 +0900

    Level1

---
+++ Programmers/Level1/수박수박수박수박수박수.py
@@ -0,0 +1,17 @@
+def solution(n):
+    tmp = "수박"
+    sol = n // 2
+    if n % 2 == 0:
+        res = tmp * sol
+    else:
+        res = (tmp*sol) + "수"
+    
+    return res
+
+# 1
+n = 3
+
+# 2
+# n = 4
+
+print(solution(n))

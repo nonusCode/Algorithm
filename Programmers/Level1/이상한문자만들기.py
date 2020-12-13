36e285124b0ca57a8584f25f1bee1cfa765e65ce
nonusCode <nonuscode@gmail.com>
2020-12-13 23:35:10 +0900

    Level1

---
+++ Programmers/Level1/이상한문자만들기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    s = s.split(' ')
+    res = ""
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if j % 2 == 0:
+                res += s[i][j].upper()
+            else:
+                res += s[i][j].lower()
+        res += res.join(' ')
+    
+    return res[:-1]
+
+# 1
+s = "try hello world"
+
+print(solution(s))

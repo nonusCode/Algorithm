a192a96bf362c8630e8dd0498c2111369d27768d
nonusCode <nonuscode@gmail.com>
2020-12-18 18:41:58 +0900

    Level1

---
+++ Programmers/Level1/핸드폰번호가리기.py
@@ -0,0 +1,12 @@
+def solution(phone_number):
+    res = len(phone_number[:-4]) * '*' + phone_number[-4:]
+    
+    return res
+
+# 1
+phone_number = "01033334444"
+
+# 2
+# phone_number = "027778888"
+
+print(solution(phone_number))
